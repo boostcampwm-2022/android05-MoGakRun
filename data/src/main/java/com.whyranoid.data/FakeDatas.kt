9e0f00526ec065173b880c7caaafce6675cb493a
soopeach <hsjeon584@gmail.com>
2022-11-16 18:49:23 +0900

    :sparkles: 파이어베이스 User 코드 완료

---
+++ data/src/main/java/com.whyranoid.data/FakeDatas.kt
@@ -0,0 +1,10 @@
+package com.whyranoid.data
+
+import FirebaseUser
+
+val fakeUser = FirebaseUser(
+    "페이크",
+    "페이크 유알엘",
+    "아이디",
+    "집에 가고 싶습니다.".split(" ")
+)
--- data/src/main/java/com.whyranoid.data/FirebaseUsers.kt
+++ data/src/main/java/com.whyranoid.data/FirebaseUsers.kt
@@ -1,59 +1,50 @@
+import com.google.firebase.firestore.FieldValue
 import com.whyranoid.data.db
+import com.whyranoid.data.fakeUser
 import kotlinx.coroutines.tasks.await
 
-// 유저 등록
-fun writeUser() {
-    val uid = "123123123"
-    db.collection("Users")
-        .document(uid).set(
-            User(
-                "신승민",
-                "집주소",
-                uid,
-                listOf("그룹", "가입", "못하죠")
-            )
-        )
-}
+val uid = "123123123"
 
-// uid로 유저 읽기
-suspend fun readUser() {
-    val uid = "123123123"
-    val userData = db.collection("Users")
-        .document(uid)
-        .get()
-        .await()
-
-//    val hi = userData.toObject((FirebaseUser::class.java))
-    println(
-        "유저 ${userData.data?.values}"
-    )
-//        .toObject((FirebaseUser::class.java))
-}
-
-data class User(
+data class FirebaseUser(
     val name: String = "",
     val profileUrl: String = "",
     val uid: String = "",
     val joinedGroupList: List<String> = emptyList()
 )
 
-data class FirebaseUser(
-    val name: String = "",
-    val profileUrl: String = "",
-    val uid: String = "",
-    val joinedGroupList: String = ""
-)
+// 유저 등록
+fun writeUser() {
+    db.collection("Users")
+        .document(uid).set(
+            FirebaseUser(
+                "신승민",
+                "집주소",
+                uid,
+                arrayListOf("그룹", "가입", "못하죠")
+            )
+        )
+}
 
-fun FirebaseUser.toUser(): User {
-    return User(
-        this.name,
-        this.profileUrl,
-        this.uid,
-        this.joinedGroupList
-            .replace("[", "")
-            .replace("]", "")
-            .split(",").map {
-                it.trim()
-            }
-    )
+// uid로 유저 읽기
+suspend fun readUser(): FirebaseUser {
+    val userData = db.collection("Users")
+        .document(uid)
+        .get()
+        .await()
+
+    return userData.toObject((FirebaseUser::class.java)) ?: fakeUser
+}
+
+// 유저가 그룹에 가입함
+fun userJoinGroup() {
+    db.collection("Users")
+        .document(uid)
+        .update("joinedGroupList", FieldValue.arrayUnion("현수 그룹"))
+}
+
+// 유저가 그룹에서 나감
+fun userExitGroup() {
+    db.collection("Users")
+        .document(uid)
+        .update("joinedGroupList", FieldValue.arrayRemove("가입"))
 }
