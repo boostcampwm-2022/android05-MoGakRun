841c06e0a713066029d2b1f82b2346cc4b49f1e7
Seungmin <tmdals8798@gmail.com>
2022-11-17 17:03:12 +0900

    ✨ dependency에 navigation 추가, navigation graph 추가 및 세팅 완료

--- presentation/src/main/java/com/whyranoid/presentation/MainActivity.kt
+++ presentation/src/main/java/com/whyranoid/presentation/MainActivity.kt
@@ -2,10 +2,15 @@ package com.whyranoid.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
