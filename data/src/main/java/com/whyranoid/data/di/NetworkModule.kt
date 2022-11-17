3fb3978a642e9a2e10b3de8de29fb59dbad72e5d
soopeach <hsjeon584@gmail.com>
2022-11-17 21:19:15 +0900

    :sparkles: Firestore DI 모듈  추가

---
+++ data/src/main/java/com/whyranoid/data/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.whyranoid.data.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideFireStoreDatabase(): FirebaseFirestore =
+        Firebase.firestore
+}
