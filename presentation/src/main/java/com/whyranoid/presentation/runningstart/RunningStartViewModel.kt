4b1af0ab282352e5d3d3b876c1b97f4942a9dc1f
yonghanJu <ju990828@naver.com>
2022-11-21 17:13:36 +0900

    :sparkles: RunningStartViewModel 에 네트워크 상태 확인 변수 추가
    Co-authored-by: bngsh <hee132@korea.ac.kr>

--- presentation/src/main/java/com/whyranoid/presentation/runningstart/RunningStartViewModel.kt
+++ presentation/src/main/java/com/whyranoid/presentation/runningstart/RunningStartViewModel.kt
@@ -2,13 +2,17 @@ package com.whyranoid.presentation.runningstart
 
 import androidx.lifecycle.ViewModel
 import com.whyranoid.domain.usecase.GetRunnerCountUseCase
+import com.whyranoid.presentation.util.networkconnection.NetworkConnectionStateHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RunningStartViewModel @Inject constructor(
-    getRunnerCountUseCase: GetRunnerCountUseCase
+    getRunnerCountUseCase: GetRunnerCountUseCase,
+    networkConnectionStateHolder: NetworkConnectionStateHolder
 ) : ViewModel() {
+
+    val networkState = networkConnectionStateHolder.networkState
 
     val runnerCount = getRunnerCountUseCase()
 }
