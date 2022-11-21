0a2b7ea104c26390f7849bb7462b05c3d814b272
yonghanJu <ju990828@naver.com>
2022-11-21 17:14:12 +0900

    :sparkles: 네트워크 상태 바인딩 어뎁터 추가
    Co-authored-by: bngsh <hee132@korea.ac.kr>

---
+++ presentation/src/main/java/com/whyranoid/presentation/util/BindingAdapters.kt
@@ -0,0 +1,14 @@
+package com.whyranoid.presentation.util
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import com.whyranoid.presentation.util.networkconnection.NetworkState
+
+@BindingAdapter("networkConnectionVisibility")
+fun View.networkConnectionVisibility(networkState: NetworkState) {
+    visibility = when (networkState) {
+        is NetworkState.UnInitialized -> View.GONE
+        is NetworkState.Connection -> View.GONE
+        is NetworkState.DisConnection -> View.VISIBLE
+    }
+}
