b65b20fc8dac102d87a17cbb52d7ceaf6a2278c3
bngsh <hee132@korea.ac.kr>
2022-11-21 22:07:34 +0900

    :sparkles: RunningStartFragment 이벤트 처리
    Co-authored-by: yonghanJu <ju990828@naver.com>

---
+++ presentation/src/main/java/com/whyranoid/presentation/running/RunningActivity.kt
@@ -0,0 +1,12 @@
+package com.whyranoid.presentation.running
+
+import com.whyranoid.presentation.R
+import com.whyranoid.presentation.base.BaseActivity
+import com.whyranoid.presentation.databinding.ActivityRunningBinding
+
+internal class RunningActivity : BaseActivity<ActivityRunningBinding>(R.layout.activity_running) {
+
+    // TODO 1. api 연동
+    // TODO 2. ui 만들기
+    // TODO 3. workmanager 사용하기
+}
--- presentation/src/main/java/com/whyranoid/presentation/runningstart/RunningStartViewModel.kt
+++ presentation/src/main/java/com/whyranoid/presentation/runningstart/RunningStartViewModel.kt
@@ -1,13 +1,19 @@
 package com.whyranoid.presentation.runningstart
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.whyranoid.domain.usecase.GetRunnerCountUseCase
+import com.whyranoid.domain.usecase.StartRunningUseCase
 import com.whyranoid.presentation.util.networkconnection.NetworkConnectionStateHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RunningStartViewModel @Inject constructor(
+    private val startRunningUseCase: StartRunningUseCase,
     getRunnerCountUseCase: GetRunnerCountUseCase,
     networkConnectionStateHolder: NetworkConnectionStateHolder
 ) : ViewModel() {
@@ -15,4 +21,23 @@ class RunningStartViewModel @Inject constructor(
     val networkState = networkConnectionStateHolder.networkState
 
     val runnerCount = getRunnerCountUseCase()
+
+    private val _eventFlow = MutableSharedFlow<Event>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun onStartButtonClicked() {
+        viewModelScope.launch {
+            if (startRunningUseCase()) {
+                emitEvent(Event.RunningStartButtonClick)
+            } else {
+                // TODO: 러닝 시작 실패 피드백 주기
+            }
+        }
+    }
+
+    private fun emitEvent(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
 }
