24040c9507d3a69e7b4e20ac341472a4d715a8bb
yonghanJu <ju990828@naver.com>
2022-11-23 19:06:34 +0900

    :sparkles: 네이버 맵 연동 화면 초기 구현
    Co-authored-by: bngsh <hee132@korea.ac.kr>

--- presentation/src/main/java/com/whyranoid/presentation/running/RunningActivity.kt
+++ presentation/src/main/java/com/whyranoid/presentation/running/RunningActivity.kt
@@ -1,12 +1,75 @@
 package com.whyranoid.presentation.running
 
+import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import com.whyranoid.presentation.R
 import com.whyranoid.presentation.base.BaseActivity
 import com.whyranoid.presentation.databinding.ActivityRunningBinding
 
-internal class RunningActivity : BaseActivity<ActivityRunningBinding>(R.layout.activity_running) {
+internal class RunningActivity :
+    BaseActivity<ActivityRunningBinding>(R.layout.activity_running), OnMapReadyCallback {
 
-    // TODO 1. api 연동
-    // TODO 2. ui 만들기
-    // TODO 3. workmanager 사용하기
+    private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mapView = binding.mapView
+        mapView
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        Marker()
+
+        naverMap.moveCamera(CameraUpdate.scrollAndZoomTo(LatLng(37.498095, 127.027610), 15.0))
+        naverMap.uiSettings.isLocationButtonEnabled = true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
