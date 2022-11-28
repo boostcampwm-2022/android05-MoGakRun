6ae61001d6af30e5d63c015beb3830a3755c7549
bngsh <hee132@korea.ac.kr>
2022-11-28 20:59:56 +0900

    :sparkles: RunningPosition을 LatLng으로 전환하는 확장 함수
    Co-authored-by: yonghanJu <ju990828@naver.com>

--- presentation/src/main/java/com/whyranoid/presentation/running/RunningPosition.kt
+++ presentation/src/main/java/com/whyranoid/presentation/running/RunningPosition.kt
@@ -1,6 +1,15 @@
 package com.whyranoid.presentation.running
 
+import com.naver.maps.geometry.LatLng
+
 data class RunningPosition(
     val latitude: Double,
     val longitude: Double
 )
+
+fun RunningPosition.toLatLng(): LatLng {
+    return LatLng(
+        this.latitude,
+        this.longitude
+    )
+}
