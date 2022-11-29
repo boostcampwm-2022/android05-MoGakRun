b528fb080f11fc6b1cd3065b3b74a49c68997400
Seungmin <tmdals8798@gmail.com>
2022-11-29 21:39:47 +0900

    :sparkles: 운동기록 페이지에서 선택한 운동기록 safe args로 가져와서 인증글 작성페이지에서 띄워주기

--- presentation/src/main/java/com/whyranoid/presentation/community/runningpost/CreateRunningPostFragment.kt
+++ presentation/src/main/java/com/whyranoid/presentation/community/runningpost/CreateRunningPostFragment.kt
@@ -1,8 +1,24 @@
 package com.whyranoid.presentation.community.runningpost
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.whyranoid.presentation.R
 import com.whyranoid.presentation.base.BaseFragment
 import com.whyranoid.presentation.databinding.FragmentCreateRunningPostBinding
 
 internal class CreateRunningPostFragment :
-    BaseFragment<FragmentCreateRunningPostBinding>(R.layout.fragment_create_running_post)
+    BaseFragment<FragmentCreateRunningPostBinding>(R.layout.fragment_create_running_post) {
+
+    private val args: CreateRunningPostFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.selectedRunningHistory = args.selectedRunningHistory
+    }
+}
