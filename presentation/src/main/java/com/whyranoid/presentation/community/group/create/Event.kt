549cc0557739c2ad092a9cf8d17040cb0cc4c9a5
soopeach <hsjeon584@gmail.com>
2022-11-30 17:41:06 +0900

    :sparkles: 그룹 생성 전 중복확인을 반드시 하도록 변경

--- presentation/src/main/java/com/whyranoid/presentation/community/group/create/Event.kt
+++ presentation/src/main/java/com/whyranoid/presentation/community/group/create/Event.kt
@@ -4,4 +4,5 @@ sealed class Event {
     data class CreateGroupButtonClick(val isSuccess: Boolean = true) : Event()
     object WarningButtonClick : Event()
     object AddRuleButtonClick : Event()
+    data class DuplicateCheckButtonClick(val isDuplicatedGroupName: Boolean = false) : Event()
 }
