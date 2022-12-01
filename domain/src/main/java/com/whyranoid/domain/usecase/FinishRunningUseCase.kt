c5b1d201aae07bc0786179c958904f298e7f0381
bngsh <hee132@korea.ac.kr>
2022-12-01 17:01:24 +0900

    :sparkles: FinishRunningUseCase에 러닝 히스토리 서버 업로드 구현

--- domain/src/main/java/com/whyranoid/domain/usecase/FinishRunningUseCase.kt
+++ domain/src/main/java/com/whyranoid/domain/usecase/FinishRunningUseCase.kt
@@ -4,12 +4,14 @@ import com.whyranoid.domain.model.RunningHistory
 import com.whyranoid.domain.repository.AccountRepository
 import com.whyranoid.domain.repository.GroupRepository
 import com.whyranoid.domain.repository.RunnerRepository
+import com.whyranoid.domain.repository.RunningHistoryRepository
 import javax.inject.Inject
 
 class FinishRunningUseCase @Inject constructor(
     private val runnerRepository: RunnerRepository,
     private val accountRepository: AccountRepository,
-    private val groupRepository: GroupRepository
+    private val groupRepository: GroupRepository,
+    private val runningHistoryRepository: RunningHistoryRepository
 ) {
     suspend operator fun invoke(runningHistory: RunningHistory? = null): Boolean {
         val uid = accountRepository.getUid()
@@ -17,6 +19,12 @@ class FinishRunningUseCase @Inject constructor(
         runnerRepository.finishRunning(uid)
 
         if (runningHistory != null) {
+            val uploadResult = runningHistoryRepository.uploadRunningHistory(runningHistory)
+
+            if (uploadResult.isFailure) {
+                return false
+            }
+
             groupRepository.getMyGroupList(uid).onSuccess { groupInfos ->
                 groupRepository.notifyRunningFinish(
                     uid = uid,
