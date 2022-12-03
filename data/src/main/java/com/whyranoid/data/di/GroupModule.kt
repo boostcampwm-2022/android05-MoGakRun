0cab66b3e61d17b7aab86706e3423a760c771f0d
soopeach <hsjeon584@gmail.com>
2022-12-03 16:49:40 +0900

    :sparkles: GroupNotificationDataSource 추상화

--- data/src/main/java/com/whyranoid/data/di/GroupModule.kt
+++ data/src/main/java/com/whyranoid/data/di/GroupModule.kt
@@ -3,6 +3,8 @@ package com.whyranoid.data.di
 import com.whyranoid.data.group.GroupDataSource
 import com.whyranoid.data.group.GroupDataSourceImpl
 import com.whyranoid.data.group.GroupRepositoryImpl
+import com.whyranoid.data.groupnotification.GroupNotificationDataSource
+import com.whyranoid.data.groupnotification.GroupNotificationDataSourceImpl
 import com.whyranoid.domain.repository.GroupRepository
 import dagger.Binds
 import dagger.Module
@@ -15,6 +17,9 @@ abstract class GroupModule {
 
     @Binds
     abstract fun bindGroupDataSource(groupDataSourceImpl: GroupDataSourceImpl): GroupDataSource
+
+    @Binds
+    abstract fun bindGroupNotificationDataSource(groupNotificationDataSourceImpl: GroupNotificationDataSourceImpl): GroupNotificationDataSource
 
     @Binds
     abstract fun bindGroupRepository(groupRepositoryImpl: GroupRepositoryImpl): GroupRepository
