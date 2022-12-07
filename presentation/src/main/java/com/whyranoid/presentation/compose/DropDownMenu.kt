7290e23b5eaab5805f3736bcc442af5536993463
soopeach <hsjeon584@gmail.com>
2022-12-07 15:39:54 +0900

    :lipstick: rule picker 레이아웃 변경

--- presentation/src/main/java/com/whyranoid/presentation/compose/DropDownMenu.kt
+++ presentation/src/main/java/com/whyranoid/presentation/compose/DropDownMenu.kt
@@ -1,7 +1,11 @@
 package com.whyranoid.presentation.compose
 
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
@@ -11,7 +15,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.whyranoid.presentation.R
 
 @Composable
@@ -19,17 +26,29 @@ fun DateDropDownMenu(
     selectedDate: String,
     onDateSelected: (String) -> Unit
 ) {
+    val dateText = stringResource(id = R.string.text_date)
     val dateList = stringArrayResource(id = R.array.rule_date_array)
     var isDropDownMenuExpanded by remember { mutableStateOf(false) }
     Button(
-        onClick = { isDropDownMenuExpanded = true }
+        onClick = { isDropDownMenuExpanded = true },
+        colors = ButtonDefaults
+            .outlinedButtonColors(
+                contentColor = colorResource(id = R.color.mogakrun_on_primary)
+            ),
+        shape = RoundedCornerShape(30),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 2.dp,
+            pressedElevation = 4.dp,
+            disabledElevation = 0.dp
+        )
     ) {
-        Text(text = selectedDate)
+        Text(text = selectedDate + dateText)
     }
 
     DropdownMenu(
         modifier = Modifier
-            .wrapContentSize(),
+            .wrapContentWidth()
+            .wrapContentHeight(),
         expanded = isDropDownMenuExpanded,
         onDismissRequest = { isDropDownMenuExpanded = false }
     ) {
@@ -38,7 +57,7 @@ fun DateDropDownMenu(
                 onDateSelected(date)
                 isDropDownMenuExpanded = false
             }) {
-                Text(text = date)
+                Text(text = date + dateText)
             }
         }
     }
@@ -49,17 +68,29 @@ fun HourDropDownMenu(
     selectedHour: String,
     onHourSelected: (String) -> Unit
 ) {
+    val hourText = stringResource(id = R.string.text_hour)
     val hourList = stringArrayResource(id = R.array.rule_hour_array)
     var isDropDownMenuExpanded by remember { mutableStateOf(false) }
     Button(
-        onClick = { isDropDownMenuExpanded = true }
+        onClick = { isDropDownMenuExpanded = true },
+        colors = ButtonDefaults
+            .outlinedButtonColors(
+                contentColor = colorResource(id = R.color.mogakrun_on_primary)
+            ),
+        shape = RoundedCornerShape(30),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 2.dp,
+            pressedElevation = 4.dp,
+            disabledElevation = 0.dp
+        )
     ) {
-        Text(text = selectedHour)
+        Text(text = selectedHour + hourText)
     }
 
     DropdownMenu(
         modifier = Modifier
-            .wrapContentSize(),
+            .wrapContentWidth()
+            .height(500.dp),
         expanded = isDropDownMenuExpanded,
         onDismissRequest = { isDropDownMenuExpanded = false }
     ) {
@@ -68,7 +99,7 @@ fun HourDropDownMenu(
                 onHourSelected(hour)
                 isDropDownMenuExpanded = false
             }) {
-                Text(text = hour)
+                Text(text = hour + hourText)
             }
         }
     }
@@ -79,18 +110,30 @@ fun MinuteDropDownMenu(
     selectedMinute: String,
     onMinuteSelected: (String) -> Unit
 ) {
+    val minuteText = stringResource(id = R.string.text_minute)
     val minuteList = stringArrayResource(id = R.array.rule_minute_array)
     var isDropDownMenuExpanded by remember { mutableStateOf(false) }
 
     Button(
-        onClick = { isDropDownMenuExpanded = true }
+        onClick = { isDropDownMenuExpanded = true },
+        colors = ButtonDefaults
+            .outlinedButtonColors(
+                contentColor = colorResource(id = R.color.mogakrun_on_primary)
+            ),
+        shape = RoundedCornerShape(30),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 2.dp,
+            pressedElevation = 4.dp,
+            disabledElevation = 0.dp
+        )
     ) {
-        Text(text = selectedMinute)
+        Text(text = selectedMinute + minuteText)
     }
 
     DropdownMenu(
         modifier = Modifier
-            .wrapContentSize(),
+            .wrapContentWidth()
+            .height(500.dp),
         expanded = isDropDownMenuExpanded,
         onDismissRequest = { isDropDownMenuExpanded = false }
     ) {
@@ -99,7 +142,7 @@ fun MinuteDropDownMenu(
                 onMinuteSelected(minute)
                 isDropDownMenuExpanded = false
             }) {
-                Text(text = minute)
+                Text(text = minute + minuteText)
             }
         }
     }
