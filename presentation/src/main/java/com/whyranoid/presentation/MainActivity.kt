4bbbdfcb02eff0c9d62fd8bff5a6856936824448
bngsh <hee132@korea.ac.kr>
2022-12-08 18:31:38 +0900

    :bookmark: local.properties missing 에러 해결

--- presentation/src/main/java/com/whyranoid/presentation/MainActivity.kt
+++ presentation/src/main/java/com/whyranoid/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.naver.maps.map.NaverMapSdk
 import com.whyranoid.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        NaverMapSdk.getInstance(applicationContext).client =
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_MAP_CLIENT_ID)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
